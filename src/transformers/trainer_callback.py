3cc84644ee23143f6af8cbbcbd0ed611b8fc550f
Nikhil Haas <nsh87@comcast.net>
2023-03-12 12:13:47 -0700

    Add debug to JSON dumps on trainer callback - Erroring in Ray

--- src/transformers/trainer_callback.py
+++ src/transformers/trainer_callback.py
@@ -94,6 +94,9 @@ class TrainerState:
 
     def save_to_json(self, json_path: str):
         """Save the content of this instance in JSON format inside `json_path`."""
+        print('Dorkus')
+        print(dataclasses)
+        print(dataclasses.asdict(self))
         json_string = json.dumps(dataclasses.asdict(self), indent=2, sort_keys=True) + "\n"
         with open(json_path, "w", encoding="utf-8") as f:
             f.write(json_string)
